455a30668767aa99ee15004ab67d2eaf65f4576a
kafka050 <kafka050@umn.edu>
2023-04-17 18:34:32 -0500

    Plant Card Functional given props

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,7 +1,15 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
+import Image from "next/image"
+import Link from "next/link"
 
 export default function PlantCard(props) {
+
+    let lightLevel = <Image src={'/sun.png'} fill css={css`object-fit: cover;`}></Image>
+    if (props.lightLevel == 'medium') 
+        lightLevel = <Image src={'/sunAndCloud.png'} fill css={css`object-fit: cover;`}></Image>
+    else if (props.lightLevel == 'low') 
+        lightLevel = <Image src={'/cloud.png'} fill css={css`object-fit: cover;`}></Image>
 
     return (
         <div css={css`
@@ -9,23 +17,63 @@ export default function PlantCard(props) {
             height: 20rem;
             background-color: slategray;
             margin: 1rem;
+            border: 0.5rem solid green;
         `}>
             <div css={css`
                 width: 100%;
                 height: 50%;
-                background-color: orange;
+                position: relative;
+                border-bottom: 0.25rem solid green;
             `}>
-                {/*Image goes here*/}
+                <Image src={props.image || '/default_plant.png'} fill></Image>
             </div>
             <div css={css`
                 width: 100%;
                 height: 50%;
-                background-color: blue;
             `}>
-                <h2 id='plantName'>{props.name}</h2>
-                <div id='waterSchedule'>
-                    {/*<Image>WATER IMAGE???</Image>*/}
-                    {/*<p> X days </p>*/}
+                <Link href={`/plants/${props.id}`}>
+                    <h3 id='plantName' css={css`height: 20%; text-align: center;`}>{props.name || 'Unnamed Plant'}</h3>
+                </Link>
+                <div css={css`
+                    display: flex;
+                    flex-direction: row;
+                    height: 80%;
+                `}>
+                    <div id='leftColumn' css={css`
+                        display: flex;
+                        flex-direction: column;
+                        flex: 1;
+                        
+                    `}>
+                        <div css={css`
+                            height: 50%;
+                            position: relative;
+                            overflow: hidden;
+                        `}>
+                            {lightLevel}
+                        </div>
+                        <div css={css`
+                            height: 50%;
+                            position: relative;
+                            text-align: center;
+                        `}>
+                            {props.tempLevel || '60-80'}
+                        </div>
+                    </div>
+                    <div id='rightColumn' css={css`
+                        display: flex;
+                        flex-direction: column;
+                        flex: 1;
+                    `}>
+                        <div css={css`
+                            height: 50%;
+                            position: relative;
+                            overflow: hidden;
+                        `}>
+                            <Image src='/watering_can.png' fill css={css`object-fit: cover;`}></Image>
+                        </div>
+                        <p css={css`text-align: center;`}> {props.lastWatered || 'today'} </p>
+                    </div>
                 </div>
             </div>
         </div>
