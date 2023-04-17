f11f324fdbdb62f49ab79214426380599445dfcf
jhettman <jhettco500@gmail.com>
2023-04-17 10:38:47 -0500

    added basic auth

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
-
+import {SignInButton, useAuth, SignedIn, SignedOut} from '@clerk/nextjs'
 export default function TopBar() {
+    const { isLoaded, userId, sessionId, getToken } = useAuth();
     return (<div css={css`
         position: fixed;
         top: 0;
@@ -32,6 +33,24 @@ export default function TopBar() {
             justify-content: center;
         `}>
             <p css={css`margin: auto;`}>Menu</p>
+        </div>
+        <div css={css`
+            flex: 1;
+            display: flex;
+            justify-content: center;
+        `}>
+        
+        <SignedOut>
+            <SignInButton mode="modal">
+                <button className="btn">
+                    Sign up
+                </button>
+            </SignInButton>
+        </SignedOut>
+        <SignedIn>
+            <p css={css`margin: auto;`}>Hello User</p>
+        </SignedIn>
+        
         </div>
     </div>)
 }
