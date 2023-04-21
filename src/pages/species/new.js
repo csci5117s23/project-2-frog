5fe510fff45612a03c4b35bdadb474ccfa7e54de
kafka050 <kafka050@umn.edu>
2023-04-21 13:52:37 -0500

    Changed waterLevel to number

--- src/pages/species/new.js
+++ src/pages/species/new.js
@@ -32,26 +32,30 @@ export default function NewSpecies() {
         <div className='field'>
             <label className='label' htmlFor='scientificName'>Scientific Name: </label>
             <div className='control'>
-                <input className='input' id='scientificName' value={scientificName} onChange={(e)=>setScientificName(e.target.value)}></input>
+                <input className='input' id='scientificName' value={scientificName} 
+                    onChange={(e)=>setScientificName(e.target.value)}></input>
             </div>
         </div>
         <div className='field'>
             <label className='label' htmlFor='commonName'>Common Name: </label>
             <div className='control'>
-                <input className='input' id='commonName' value={commonName} onChange={(e)=>setCommonName(e.target.value)}></input>
+                <input className='input' id='commonName' value={commonName} 
+                    onChange={(e)=>setCommonName(e.target.value)}></input>
             </div>
         </div>
         <div className='field'>
             <label className='label' htmlFor='description'>Description: </label>
             <div className='control'>
-                <textarea className='textarea' id='description' value={description} onChange={(e)=>setDescription(e.target.value)}></textarea>
+                <textarea className='textarea' id='description' value={description} 
+                    onChange={(e)=>setDescription(e.target.value)}></textarea>
             </div>
         </div>
         <div className='field'>
             <label htmlFor='lightLevel'>Light Level: </label>
             <div className='control'>
                 <div className='select'>
-                    <select id='lightLevel' value={lightLevel} onChange={(e)=>setLightLevel(e.target.value)}>
+                    <select id='lightLevel' value={lightLevel} 
+                        onChange={(e)=>setLightLevel(e.target.value)}>
                         <option value='high'>high</option>
                         <option value='medium'>medium</option>
                         <option value='low'>low</option>
@@ -62,13 +66,15 @@ export default function NewSpecies() {
         <div className='field'>
             <label className='label' htmlFor='wateringDays'>Days between watering: </label>
             <div className='control'>
-                <input className='input' id='wateringDays' value={wateringDays} onChange={(e)=>setWateringDays(e.target.value)}></input>
+                <input className='input' type='number' id='wateringDays' value={wateringDays} 
+                    onChange={(e)=>setWateringDays(e.target.value)}></input>
             </div>
         </div>
         <div className='field'>
             <label className='label' htmlFor='tempLevel'>Temperature Range: </label>
             <div className='control'>
-                <input className='input' id='tempLevel' value={tempLevel} onChange={(e)=>setTempLevel(e.target.value)}></input>
+                <input className='input' id='tempLevel' value={tempLevel} 
+                    onChange={(e)=>setTempLevel(e.target.value)}></input>
             </div>
         </div>
         <div className='control'>
