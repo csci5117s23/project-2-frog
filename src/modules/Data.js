c7e42ac021387543d647c323690836b2032ba76c
bomentum <55463846+bomentum@users.noreply.github.com>
2023-04-26 11:38:07 -0500

    Newplant (#14)
    * layout to Layout.js
    
    * layout to Layout.js
    
    * spot for image upload in newplant.js
    
    * added db search for _id, still need to clean it up a bit
    
    * styling & comments
    
    * minor style
    
    * arranged & added last water date
    
    * search common name and species
    
    * add plant, need to work on image upload
    
    * cleaned up code, added default for date error submission, changed class to className
    
    * css

--- src/modules/Data.js
+++ src/modules/Data.js
@@ -1,88 +1,97 @@
-const backend_base = process.env.NEXT_PUBLIC_BACKEND_BASE_URL
-
+const backend_base = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
 
 export async function getSpecies(speciesId, token) {
     const result = await fetch(`${backend_base}/species/${speciesId}`, {
-        'method': 'GET',
-        'headers': {
-            Authorization: `Bearer: ${token}`
-        }
-    })
-    if (!result.ok) return -1
-    return await result.json()
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer: ${token}`,
+        },
+    });
+    if (!result.ok) return -1;
+    return await result.json();
+}
+
+export async function getSpeciesByName(species, token) {
+    const result = await fetch(`${backend_base}/species?species=${species}`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer: ${token}`,
+        },
+    });
+    if (!result.ok) return result.response;
+    return await result.json();
 }
 
 export async function postSpecies(speciesJSON, token) {
     const result = await fetch(`${backend_base}/species/`, {
-        'method': 'POST',
-        'headers': {
-            'Authorization': `Bearer: ${token}`,
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer: ${token}`,
             'Content-Type': 'application/json',
         },
-        'body': JSON.stringify(speciesJSON)
-    })
-    if (!result.ok) return -1
-    return await result.json()
+        body: JSON.stringify(speciesJSON),
+    });
+    if (!result.ok) return -1;
+    return await result.json();
 }
 
 export async function patchSpecies(speciesId, speciesJSON, token) {
     const result = await fetch(`${backend_base}/species/${speciesId}`, {
-        'method': 'PATCH',
-        'headers': {
-            'Authorization': `Bearer: ${token}`,
+        method: 'PATCH',
+        headers: {
+            Authorization: `Bearer: ${token}`,
             'Content-Type': 'application/json',
         },
-        'body': JSON.stringify(speciesJSON)
-    })
-    if (!result.ok) return -1
-    return await result.json()
+        body: JSON.stringify(speciesJSON),
+    });
+    if (!result.ok) return -1;
+    return await result.json();
 }
 
 export async function getPlantsForUser(token) {
     const result = await fetch(`${backend_base}/plants`, {
-        'method': 'GET',
-        'headers': {
-            'Authorization': `Bearer: ${token}`,
-        }
-    })
-    if (!result.ok) return -1
-    return await result.json()
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer: ${token}`,
+        },
+    });
+    if (!result.ok) return -1;
+    return await result.json();
 }
 
 export async function getPlantById(plantId, token) {
     const result = await fetch(`${backend_base}/plants/${plantId}`, {
-        'method': 'GET',
-        'headers': {
-            'Authorization': `Bearer: ${token}`,
-        }
-    })
-    if (!result.ok) return -1
-    return await result.json()
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer: ${token}`,
+        },
+    });
+    if (!result.ok) return -1;
+    return await result.json();
 }
-
 
 export async function postPlant(plantJSON, token) {
     const result = await fetch(`${backend_base}/plants/`, {
-        'method': 'POST',
-        'headers': {
-            'Authorization': `Bearer: ${token}`,
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer: ${token}`,
             'Content-Type': 'application/json',
         },
-        'body': JSON.stringify(plantJSON)
-    })
-    if (!result.ok) return -1
-    return await result.json()
+        body: JSON.stringify(plantJSON),
+    });
+    if (!result.ok) return -1;
+    return await result.json();
 }
 
 export async function patchPlant(plantId, plantJSON, token) {
     const result = await fetch(`${backend_base}/plants/${plantId}`, {
-        'method': 'PATCH',
-        'headers': {
-            'Authorization': `Bearer: ${token}`,
+        method: 'PATCH',
+        headers: {
+            Authorization: `Bearer: ${token}`,
             'Content-Type': 'application/json',
         },
-        'body': JSON.stringify(plantJSON)
-    })
-    if (!result.ok) return -1
-    return await result.json()
+        body: JSON.stringify(plantJSON),
+    });
+    if (!result.ok) return -1;
+    return await result.json();
 }
--- src/pages/newplant.js
+++ src/pages/newplant.js
@@ -1,105 +1,217 @@
-//Add new plant
-import { useAuth } from '@clerk/nextjs';
-import { useState, useEffect } from 'react';
-import 'bulma/css/bulma.css';
+//Add new plant page
+//https://github.com/tbleckert/react-select-search
+import { useAuth } from '@clerk/nextjs'
+import { useState, useEffect } from 'react'
+import 'bulma/css/bulma.css'
+import { getSpecies, getSpeciesByName, postPlant } from '@/modules/Data'
+import React from 'react'
+import SelectSearch from 'react-select-search'
+
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function NewPlant() {
-    //Clerk
-    const { isLoaded, userId, isSignedIn, getToken } = useAuth();
-    const [loading, setLoading] = useState(true);
+	const { isLoaded, userId, isSignedIn, getToken } = useAuth()
+	const [loading, setLoading] = useState(true)
 
-    // Get to do items that are false
-    useEffect(() => {
-        async function process() {
-            if (userId) {
-                try {
-                    //From CLERK JWT templates for authentication
-                    const token = await getToken({ template: 'codehooks' });
-                } catch (e) {
-                    console.log('error in todos useEffect::', e.message);
-                }
-            }
-            setLoading(false);
-        }
-        process();
-    }, [isLoaded]);
+	// All plants in db
+	const [speciesList, setSpeciesList] = useState([])
 
-    //Retrieve input data
-    async function submitHandler(e) {
-        e.preventDefault();
-        const data = event.target.item.value;
-        const token = await getToken({ template: 'todo' });
-        console.log('token:', token);
+	//The returned user selected species by _id
+	const [getPlant, setGetPlant] = useState([])
 
-        try {
-            console.log('try');
-        } catch (error) {
-            console.log('Error: ', error);
-        }
-    }
+	//User input for plant name, last water, and image
+	const [plantName, setPlantName] = useState('')
+	const [waterDate, setWaterDate] = useState(() => new Date())
+	const [image, setImage] = useState([])
 
-    if (loading) {
-        return (
-            <>
-                <div>loading....</div>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <form>
-                    <div class='has-text-weight-bold newPlant'>
-                        <div class='field'>
-                            <div class='label'>Common Name</div>
-                            <div class='control is-expanded'>
-                                <input class='input' type='text' id='commonName' placeholder='Add Plant Name'></input>
-                            </div>
-                        </div>
-                        <div class='field'>
-                            <div class='label'>Species</div>
-                            <div class='control is-expanded'>
-                                <input class='input' type='text' id='species' placeholder='Add Plant Species'></input>
-                            </div>
-                        </div>
-                        <div class='field'>
-                            <div class='label'>Sunlight</div>
-                            <div class='control is-expanded'>
-                                <input class='input' type='text' id='lightLevel' placeholder='Add Sunlight Care'></input>
-                                <span class='icon is-left'></span>
-                                {/* <FontAwesomeIcon icon='fa-light fa-sun' /> */}
-                            </div>
-                        </div>
-                        <div class='field'>
-                            <div class='label'>Water</div>
-                            <div class='control is-expanded'>
-                                <input class='input' type='text' id='waterLevel' placeholder='Add Water Care'></input>
-                                <span class='icon is-left'></span>
-                                {/* add icon */}
-                            </div>
-                        </div>
-                        <div class='field'>
-                            <div class='label'>Temperature</div>
-                            <div class='control is-expanded'>
-                                <input class='input' type='text' id='tempLevel' placeholder='Add Temperature Care'></input>
-                                <span class='icon is-left'></span>
-                                {/* add icon */}
-                            </div>
-                        </div>
-                        <div class='field'>
-                            <div class='label'>Description</div>
-                            <div class='control is-expanded'>
-                                <textarea class='input' type='text' id='description' placeholder='Description'></textarea>
-                            </div>
-                        </div>
-                        <div class='field'>
-                            <div class='control'>
-                                <a class='button'>Submit</a>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </>
-        );
-    }
+	//Get all plant data from db
+	useEffect(() => {
+		async function process() {
+			if (userId) {
+				try {
+					//From CLERK JWT templates for authentication
+					const token = await getToken({
+						template: 'codehooks',
+					})
+					const list = await getSpeciesByName('', token)
+					setSpeciesList(list)
+				} catch (e) {
+					console.log('error in useEffect::', e.message)
+				}
+			}
+			setLoading(false)
+		}
+		process()
+	}, [isLoaded, loading])
+
+	//Get user selected plant from db using species id
+	async function submitSpeciesId(id) {
+		try {
+			const token = await getToken({ template: 'codehooks' })
+			const list = await getSpecies(id, token)
+			setGetPlant(list)
+		} catch (error) {
+			console.log('Error: ', error)
+		}
+	}
+
+	//style the rendering of the list, bug for arrow and tab keys
+	function renderGroup(allGroup, { stack, name }, snapshot, className) {
+		return (
+			<button {...allGroup} className={className} type='button'>
+				<span style={{ fontFamily: stack }}>{name}</span>
+			</button>
+		)
+	}
+
+	//Add new plant to plants db
+	async function addPlant(e) {
+		e.preventDefault()
+
+		try {
+			const token = await getToken({ template: 'codehooks' })
+			const list = await postPlant(
+				{
+					userId: userId,
+					name: plantName,
+					species: getPlant.species,
+					image: '/sun.png',
+					lastWatered: waterDate,
+				},
+				token
+			)
+			if (list == -1) {
+				alert('Error Posting Plant ')
+			}
+			setGetPlant(list)
+		} catch (error) {
+			console.log('Error: ', error)
+		}
+		window.location.reload()
+	}
+
+	//Separate species and common name for search filter
+	const groupCommonName = speciesList.map((specs) => ({ name: specs.commonName, value: specs._id }))
+	const groupSpecies = speciesList.map((specs) => ({ name: specs.species, value: specs._id }))
+
+	if (loading) {
+		return (
+			<>
+				<div>loading....</div>
+			</>
+		)
+	} else {
+		return (
+			<>
+				<form>
+					<div className='has-text-weight-bold newPlant'>
+						<div className='field is-grouped'>
+							<div className='label'>Select Plant </div>
+							<div className='control'>
+								<SelectSearch
+									// renderOption={renderGroup}
+									options={[
+										{
+											type: 'group',
+											name: 'Species',
+											items: groupSpecies,
+										},
+										{
+											type: 'group',
+											name: 'Common Name',
+											items: groupCommonName,
+										},
+									]}
+									id='search'
+									placeholder='Search Plants'
+									search={true}
+									type='group'
+									multiple={false}
+									onChange={(id) => submitSpeciesId(id)}
+									// autoFucus
+								></SelectSearch>
+							</div>
+							<div className='control'>
+								<button className='button is-small'>Add New Species</button>
+							</div>
+						</div>
+					</div>
+					<div className='selectedPlant'>
+						<div className='context mt-2 has-text-weight-bold'></div>
+						<ul>
+							{Object.entries(getPlant).map(function (el) {
+								const [key, value] = el
+								return (
+									<li key={key}>
+										<span className='context has-text-weight-bold'>{key}</span>{' '}
+										: {value}
+									</li>
+								)
+							})}
+						</ul>
+					</div>
+				</form>
+				<form>
+					<div className='field mt-5'>
+						<div className='label'>Name</div>
+						<div className='control is-expanded'>
+							<input
+								className='input'
+								type='text'
+								id='name'
+								placeholder='Name Your Plant'
+								onChange={(e) => {
+									setPlantName(e.target.value)
+								}}></input>
+						</div>
+					</div>
+					<div className='field'>
+						<div className='label'>Last Watered</div>
+						<div className='control is-expanded'>
+							<input
+								className='inputDate'
+								type='date'
+								id='date'
+								placeholder='Enter Date'
+								onChange={(e) => {
+									setWaterDate(new Date(e.target.value))
+								}}></input>
+						</div>
+					</div>
+					<div className='field'>
+						<div className='label'></div>
+						<figure className='image is-128x128'>
+							<img src='/sun.png'></img>
+						</figure>
+						{/* <div class='file is-small'>
+                            <label class='file-label'>
+                                <input
+                                    className='file-input'
+                                    type='file'
+                                    name='resume'
+                                    accept='image/*'
+                                    onChange={(e) => {
+                                        setImage(e.target.value);
+                                        console.log('image', image);
+                                    }}
+                                />
+                                <span class='file-cta'>
+                                    <span class='file-icon'>
+                                        <i class='fas fa-upload'></i>
+                                    </span>
+                                    <span class='file-label'>Upload</span>
+                                </span>
+                            </label>
+                        </div> */}
+					</div>
+
+					<div className='control'>
+						<button className='button is-large' onClick={addPlant} value='submit'>
+							Add Plant
+						</button>
+					</div>
+				</form>
+			</>
+		)
+	}
 }
