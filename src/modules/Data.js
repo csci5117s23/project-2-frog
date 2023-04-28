80c4a54107ffc2338fa1fa6e2711d0fa6459d13d
Joe Rendleman <rendl008@umn.edu>
2023-04-28 11:48:58 -0500

    deleted stupid colon

--- src/modules/Data.js
+++ src/modules/Data.js
@@ -4,7 +4,7 @@ export async function getSpecies(speciesId, token) {
     const result = await fetch(`${backend_base}/species/${speciesId}`, {
         method: 'GET',
         headers: {
-            Authorization: `Bearer: ${token}`,
+            Authorization: `Bearer ${token}`,
         },
     });
     if (!result.ok) return -1;
@@ -15,7 +15,7 @@ export async function getSpeciesByName(species, token) {
     const result = await fetch(`${backend_base}/species?species=${species}`, {
         method: 'GET',
         headers: {
-            Authorization: `Bearer: ${token}`,
+            Authorization: `Bearer ${token}`,
         },
     });
     if (!result.ok) return result.response;
@@ -27,7 +27,7 @@ export async function getAllSpecies(token) {
     const result = await fetch(`${backend_base}/species`, {
         method: 'GET',
         headers: {
-            Authorization: `Bearer: ${token}`,
+            Authorization: `Bearer ${token}`,
         },
     });
     if (!result.ok) return result.response;
@@ -39,7 +39,7 @@ export async function postSpecies(speciesJSON, token) {
     const result = await fetch(`${backend_base}/species/`, {
         method: 'POST',
         headers: {
-            Authorization: `Bearer: ${token}`,
+            Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(speciesJSON),
@@ -52,7 +52,7 @@ export async function patchSpecies(speciesId, speciesJSON, token) {
     const result = await fetch(`${backend_base}/species/${speciesId}`, {
         method: 'PATCH',
         headers: {
-            Authorization: `Bearer: ${token}`,
+            Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(speciesJSON),
@@ -65,7 +65,7 @@ export async function getPlantsForUser(token) {
     const result = await fetch(`${backend_base}/plants`, {
         method: 'GET',
         headers: {
-            Authorization: `Bearer: ${token}`,
+            Authorization: `Bearer ${token}`,
         },
     });
     if (!result.ok) return -1;
@@ -76,7 +76,7 @@ export async function getPlantById(plantId, token) {
     const result = await fetch(`${backend_base}/plants/${plantId}`, {
         method: 'GET',
         headers: {
-            Authorization: `Bearer: ${token}`,
+            Authorization: `Bearer ${token}`,
         },
     });
     if (!result.ok) return -1;
@@ -87,7 +87,7 @@ export async function postPlant(plantJSON, token) {
     const result = await fetch(`${backend_base}/plants/`, {
         method: 'POST',
         headers: {
-            Authorization: `Bearer: ${token}`,
+            Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(plantJSON),
@@ -100,7 +100,7 @@ export async function patchPlant(plantId, plantJSON, token) {
     const result = await fetch(`${backend_base}/plants/${plantId}`, {
         method: 'PATCH',
         headers: {
-            Authorization: `Bearer: ${token}`,
+            Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(plantJSON),
